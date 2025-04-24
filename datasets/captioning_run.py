17677b6258b7402812953eb20c3a97f51907bd39
jeongwoo1020 <154405015+jeongwoo1020@users.noreply.github.com>
2025-04-24 23:53:58 +0900

    Update and rename captioning_run.py to datasets/captioning_run.py

--- datasets/captioning_run.py
+++ datasets/captioning_run.py
@@ -6,7 +6,6 @@ openai.api_key = "YOUR_API_KEY"
 # image_base64.json에서 이미 이미지 데이터를 불러오기만 합니다.
 image_base64_data = read_base64_images('retro_image_base64.json')  # 여기서 'image_base64.json' 파일 경로를 설정하세요
 
-# 첫 10개 이미지만 선택
 selected_images = list(image_base64_data.keys()) 
 
 # 이미지 파일명 목록과 처리할 이미지 데이터 목록 설정
